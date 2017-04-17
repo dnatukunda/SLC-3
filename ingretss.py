b0c885f970de88d97353ce4aadfb2b3b09fb7641
Natukunda Dorcus <natukundadorcus@gmail.com>
2017-04-17 16:03:00 +0300

    my rep

---
+++ ingretss.py
@@ -0,0 +1,14 @@
+#print('get the ingredients')
+
+def make_breakfast(food):
+	print ("Get the ingredients")
+	print("mix")
+	print("pour into the head pan")
+	print("flip to the other side")
+	breakfast = "___ yummy {}".format(food) 
+	print(breakfast)
+
+make_breakfast("eggs")
+make_breakfast("fruits")
+make_breakfast("bread")
+
